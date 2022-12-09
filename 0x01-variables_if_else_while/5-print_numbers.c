3ae0fa459704dc743e3106681e1b31c46496f2b0
Gotodiero <mauricekonyango@gmail.com>
2022-12-09 11:29:03 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}

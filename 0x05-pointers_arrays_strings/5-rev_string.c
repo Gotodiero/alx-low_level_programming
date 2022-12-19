5745e2d6352602d05cf3dace6108b78d21dcdbb9
Gotodiero <mauricekonyango@gmail.com>
2022-12-19 07:09:04 -0800

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,16 +6,16 @@
  */
 void rev_string(char *s)
 {
-	char rev = s[0];
-	int counter = 0;
+	char rv = s[0];
+	int c = 0;
 	int i;
 
-	while (s[counter] != '\0')
-	counter++;
-	for (i = 0; i < counter; i++)
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
 	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
+		c--;
+		rv = s[i];
+		s[i] = s[c];
 	}
 }

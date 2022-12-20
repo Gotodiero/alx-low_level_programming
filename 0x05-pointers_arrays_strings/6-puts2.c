07d896dedc71549ded5a87426656a7454524e7bf
Gotodiero <mauricekonyango@gmail.com>
2022-12-19 18:41:20 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,23 +7,17 @@
  */
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o
+	int i;
 
-	while (*y != '\0')
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
+		if (i % 2 == 0)
 		{
-			_putchar(str[o]);
+			_putchar(str[i]);
 		}
+		i++;
 	}
 	_putchar('\n');
 }
